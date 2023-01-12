47a8767cc3b2b4d392643b349faa0f6789e2b687
Andres Scarafia <andres.g.scarafia@gmail.com>
2023-01-12 18:20:04 -0300

    Add script.js, create functions and edit footer

---
+++ script.js
@@ -0,0 +1,72 @@
+function encriptar(texto) {
+    console.log("ENCRIPTAR")
+    let textoEncriptado = ""
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] == "a") {
+            textoEncriptado += "ai";
+        } else if(texto[i] == "e") {
+            textoEncriptado += "enter";
+        } else if(texto[i] == "i") {
+            textoEncriptado += "imes";
+        } else if(texto[i] == "o") {
+            textoEncriptado += "ober";
+        } else if(texto[i] == "u") {
+            textoEncriptado += "ufat";
+        } else {
+            textoEncriptado += texto[i];
+        }
+    }
+    return textoEncriptado
+}
+
+function desencriptar(texto) {
+    let textoDesencriptado = ""
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] == "a") {
+            textoDesencriptado += "a";
+            i++;
+        } else if(texto[i] == "e") {
+            textoDesencriptado += "e";
+            i += 4;
+        } else if(texto[i] == "i") {
+            textoDesencriptado += "i";
+            i += 3;
+        } else if(texto[i] == "o") {
+            textoDesencriptado += "o";
+            i += 3;
+        } else if(texto[i] == "u") {
+            textoDesencriptado += "u";
+            i += 3;
+        } else {
+            textoDesencriptado += texto[i];
+        }
+    }
+    return textoDesencriptado;
+}
+
+
+/* Código relevante de JS:
+  document.write(mensaje)
+
+  parseInt("numero")
+
+  document.querySelector(css-selector)
+
+  // Aquí viene la llamada a la función con el evento onclick
+    pantalla.onclick = mostrarMensaje;
+    button.onclick = nombreFuncion;
+
+    var button = document.querySelector("button");
+	  button.onclick = verificar;
+
+
+  var input = document.querySelector("input");
+	input.focus(); mueve el cursor al input
+  input.value = ""; vacia el campo del input
+
+  pantalla.onclick = cambiarColor;
+    pantalla.onmousemove = dibujarCirculo;
+    pantalla.onmousedown = habilitarDibujar;
+    pantalla.onmouseup = deshabilitarDibujar;
+
+*/
